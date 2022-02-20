b974150284296c4761afd887da2c861d1034e812
Liogl <github@limerigg.com>
2022-02-20 12:00:54 -0500

    Update test_board_led_btn.py

--- test_board_led_btn.py
+++ test_board_led_btn.py
@@ -71,7 +71,7 @@ while True:
     try:
         for s in S:
             d = s.set_led(led_state)
-            print("Changed led to "+led_state)
+            print("Changed led to ",led_state)
         time.sleep(60)
         led_state = 0 if (led_state) else 1
     except KeyboardInterrupt:
