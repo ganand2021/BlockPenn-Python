1d73018f7614ccb40887fa113d49f34e757f94e4
Liogl <github@limerigg.com>
2022-02-20 16:40:11 -0500

    Clean and comment

--- test_board_led_btn.py
+++ test_board_led_btn.py
@@ -8,14 +8,12 @@ import lgpio as sbc
 import sys
 import argparse
 import datetime
-import signal
-import atexit
 
-LED1_PIN = 22
-LED2_PIN = 23
+LED1_PIN = 22 # green 
+LED2_PIN = 23 # red
 
-BTN1_PIN = 4
-BTN2_PIN = 27
+BTN1_PIN = 4 # Bottom, Hi by default
+BTN2_PIN = 27 # Into the center of the PCB, Hi by default
 
 """
 Has to run after 'sensor' because we run gpiochip_open and closing it in 'sensor'
@@ -39,27 +37,10 @@ class btn:
     def sts_btn(self):
         return sbc.gpio_read(self.chip, self.btn_pin)
 
-def handle_exit(*err_msg):
-#        sbc.gpiochip_close(chip)
-#        chip = sbc.gpiochip_open(args.gpiochip)
-#        led1.set_led(0)
-    sbc.gpiochip_close(chip)
-    if err_msg: 
-        print("Terminated: code("+str(err_msg[0])+")")
-        print("Error message:", err_msg[1])
-        exit(err_msg)
-    else:
-        print("DHT terminated. GPIO closed.")
-        exit(0)
-
 chip = sbc.gpiochip_open(0)
 # Instantiate a class for each GPIO
 
 # Set the led
-atexit.register(handle_exit)
-signal.signal(signal.SIGTERM, handle_exit)
-signal.signal(signal.SIGINT, handle_exit)
-
 led1 = led(chip, LED1_PIN, 0)
 led2 = led(chip, LED2_PIN, 0)
 btn1 = btn(chip, BTN1_PIN)
@@ -68,13 +49,13 @@ btn2 = btn(chip, BTN2_PIN)
 led_state = 1
 test_cnt = 0
 
-while test_cnt<2:
+while (btn1.sts_btn() or btn2.sts_btn()):
     try:
         led1.set_led(led_state)
-#        led2.set_led(led_state)
+        led2.set_led(led_state)
         print("btn1 is ", btn1.sts_btn())
-#        print("btn2 is ", btn2.sts_btn())
-        time.sleep(10)
+        print("btn2 is ", btn2.sts_btn())
+        time.sleep(1)
         led_state = 0 if (led_state) else 1
         test_cnt = test_cnt + 1
     except KeyboardInterrupt:
