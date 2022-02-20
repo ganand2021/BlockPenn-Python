5bb0f01c6dbff10e66f9df70740f00996bee789d
Liogl <github@limerigg.com>
2022-02-20 16:25:47 -0500

    Update test_board_led_btn.py

--- test_board_led_btn.py
+++ test_board_led_btn.py
@@ -14,16 +14,8 @@ import atexit
 LED1_PIN = 22
 LED2_PIN = 23
 
-DHTAUTO = 0
-DHT11 = 1
-DHTXX = 2
-
-DHT_GOOD = 0
-DHT_BAD_CHECKSUM = 1
-DHT_BAD_DATA = 2
-DHT_TIMEOUT = 3
-
-OUT=20
+BTN1_PIN = 17
+BTN2_PIN = 27
 
 """
 Has to run after 'sensor' because we run gpiochip_open and closing it in 'sensor'
@@ -38,6 +30,14 @@ class led:
     def set_led(self, state):
         sbc.gpio_write(self.chip, self.led_pin, state)
 
+class btn:
+    def __init__(self, chip, btn_pin, callback=None):
+        sbc.gpio_claim_input(chip, btn_pin)
+        self.chip = chip
+        self.btn_pin = btn_pin
+
+    def sts_btn(self):
+        return sbc.gpio_read(self.chip, self.btn_pin)
 
 def handle_exit(*err_msg):
 #        sbc.gpiochip_close(chip)
@@ -60,26 +60,24 @@ atexit.register(handle_exit)
 signal.signal(signal.SIGTERM, handle_exit)
 signal.signal(signal.SIGINT, handle_exit)
 
-S = []
-
-S.append(led(chip, LED1_PIN, 0))  
-S.append(led(chip, LED2_PIN, 0))  
+led1 = led(chip, LED1_PIN, 0)
+led2 = led(chip, LED2_PIN, 0)
+btn1 = btn(chip, BTN1_PIN)
+btn2 = btn(chip, BTN2_PIN)
 
 led_state = 1
 test_cnt = 0
 
 while test_cnt<2:
     try:
-        for s in S:
-            d = s.set_led(led_state)
-            print("Changed led to ",led_state)
+        led1.set_led(led_state)
+        led2.set_led(led_state)
+        print("btn1 is ", btn1.sts_btn)
+        print("btn2 is ", btn2.sts_btn)
         time.sleep(10)
         led_state = 0 if (led_state) else 1
         test_cnt = test_cnt + 1
     except KeyboardInterrupt:
         break
-
-for s in S:
-    s.cancel()
 
 sbc.gpiochip_close(chip)
