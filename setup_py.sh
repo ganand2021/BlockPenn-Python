f508c4c21bd67f6a1a0de982d963caba6fc04493
g1ibby <me@appci.xyz>
2022-03-29 19:59:18 +0700

    feat: setup script

--- setup_py.sh
+++ setup_py.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
 echo "Setting up RPi Python scripts environment for the first time"
-`sudo apt install python3.9-venv`
-`python3 -m venv .`
-`sudo apt install python3-pip`
-`sudo pip3 install influxdb`
-`sudo apt install python3-lgpio`
-`sudo apt-get install libgpiod2`
+
+sudo -u ubuntu apt install python3-pip
+sudo -u ubuntu apt install python3-lgpio
+sudo -u ubuntu apt-get install libgpiod2
+sudo -u ubuntu apt install -y python3-rpi.gpio
+sudo -u ubuntu apt install -y python3-pil
+
+python3 -m venv .
+source ./venv/bin/activate
+pip3 install influxdb
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
+cd Adafruit_Python_SSD1306
+pip3 install Adafruit_Python_SSD1306
+python3 setup.py install
+cd ..
+pip3 install adafruit-circuitpython-shtc3
