b6a57ff59a0357787e0cc2aff923a11cb6928fa3
Liogl <github@limerigg.com>
2022-04-10 16:31:36 -0400

    Update sensor_start_w_sps.py

--- sensor_start_w_sps.py
+++ sensor_start_w_sps.py
@@ -387,6 +387,6 @@ if __name__ == "__main__":
 			# red_led.set_led(1)
 			# GPIO.cleanup()
 			logging.exception("main crashed. Error: %s", e)
-			wait(15)
+			wait(10)
 	green_led.set_led(0)
- 	GPIO.cleanup()
+	GPIO.cleanup()
