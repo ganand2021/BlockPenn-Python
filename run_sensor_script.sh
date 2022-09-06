2e4e67683a901b003fd2a1760f1c4c9991b36385
Liogl <55851770+Liogl@users.noreply.github.com>
2022-09-05 22:51:44 -0400

    Update run_sensor_script.sh

--- run_sensor_script.sh
+++ run_sensor_script.sh
@@ -6,7 +6,7 @@ do
     sleep 5
 done
 echo "Influx is running, starting sensors python script"
-cd /home/ubuntu/Blockpenn-Python         
+cd /home/ubuntu/BlockPenn-Python         
 while true
 do
     sudo python3 sensor_start_w_sps_v2.py
