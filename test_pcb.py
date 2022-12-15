b97c39b09a873eaeaf3befcbda02477671bf7716
Liogl <github@limerigg.com>
2022-12-15 15:16:12 +0200

    Update test_pcb.py

--- test_pcb.py
+++ test_pcb.py
@@ -52,20 +52,20 @@ logging.basicConfig(
 logger.debug('Script started')
 
 class bcolors:
-    TEXT = '\033[0m'
-    PASS = '\033[32m'
-    WARN = '\033[33m'
-    FAIL = '\033[31m'
-    HIGH = '\033[36m'
+	TEXT = '\033[0m'
+	PASS = '\033[32m'
+	WARN = '\033[33m'
+	FAIL = '\033[31m'
+	HIGH = '\033[36m'
 
 # Start the lgpio
 def gpio_start():
-    # for the leds and buttons
-    global GPIO 
-    GPIO.setwarnings(False) # Ignore warning (TBD)
-    GPIO.setmode(GPIO.BCM) # Use BCM instead of physical mapping
-    global i2c 
-    i2c = board.I2C()  # uses board.SCL and board.SDA
+	# for the leds and buttons
+	global GPIO 
+	GPIO.setwarnings(False) # Ignore warning (TBD)
+	GPIO.setmode(GPIO.BCM) # Use BCM instead of physical mapping
+	global i2c 
+	i2c = board.I2C()  # uses board.SCL and board.SDA
 
 # GPIO classes: led & btn
 class led:
@@ -88,91 +88,91 @@ def button_callback(channel):
 	print("Button was pushed! (GPIO "+str(channel)+")")
 
 def test(comp_test):
-    res_test = False
+	res_test = False
 
-    if comp_test == "SHTC3":
-        print("Testing SHTC3")
-        try: 
-            test_shtc3()
-            res_test = True
-        except: res_test = False
-    elif comp_test == "SPS30":
-        print("Testing SPS30")
-        try: 
-            test_sps30()
-            res_test = True
-        except: res_test = False
-    elif comp_test == "T6713":
-        print("Testing T6713")
-        try: 
-            test_t6713()
-            res_test = True
-        except: res_test = False
-    elif comp_test == "OLED":
-        print("Testing OLED")
-        try: 
-            test_oled()
-            res_test = True
-        except: res_test = False
-    else:
-        print("Error: could not find component function")
+	if comp_test == "SHTC3":
+		print("Testing SHTC3")
+		try: 
+			test_shtc3()
+			res_test = True
+		except: res_test = False
+	elif comp_test == "SPS30":
+		print("Testing SPS30")
+		try: 
+			test_sps30()
+			res_test = True
+		except: res_test = False
+	elif comp_test == "T6713":
+		print("Testing T6713")
+		try: 
+			test_t6713()
+			res_test = True
+		except: res_test = False
+	elif comp_test == "OLED":
+		print("Testing OLED")
+		try: 
+			test_oled()
+			res_test = True
+		except: res_test = False
+	else:
+		print("Error: could not find component function")
 
-    return res_test
+	return res_test
 
 def test_shtc3():
-    # Connect SHTC3
-    import adafruit_shtc3
-    sht = adafruit_shtc3.SHTC3(i2c)
-    temperature, relative_humidity = sht.measurements
-    sh_temp = str("Temperature: %0.1f C" % temperature)
-    sh_humi = str("Humidity: %0.1f %%" % relative_humidity)
-    print(f"Read SHTC3: {sh_temp}, {sh_humi}")
+	# Connect SHTC3
+	import adafruit_shtc3
+	sht = adafruit_shtc3.SHTC3(i2c)
+	temperature, relative_humidity = sht.measurements
+	sh_temp = str("Temperature: %0.1f C" % temperature)
+	sh_humi = str("Humidity: %0.1f %%" % relative_humidity)
+	print(f"Read SHTC3: {sh_temp}, {sh_humi}")
 
 def test_sps30():
-    import sps30
-    global sps
-    sps = sps30.SPS30(1)
-    if sps.read_article_code() == sps.ARTICLE_CODE_ERROR:
-        raise Exception("SPS30: ARTICLE CODE CRC ERROR!")
-    else:
-        print("SPS30: ARTICLE CODE: " + str(sps.read_article_code()))
+	import sps30
+	global sps
+	sps = sps30.SPS30(1)
+	if sps.read_article_code() == sps.ARTICLE_CODE_ERROR:
+		raise Exception("SPS30: ARTICLE CODE CRC ERROR!")
+	else:
+		print("SPS30: ARTICLE CODE: " + str(sps.read_article_code()))
 
-    if sps.read_device_serial() == sps.SERIAL_NUMBER_ERROR:
-        raise Exception("SPS30: SERIAL NUMBER CRC ERROR!")
-    else:
-        print("SPS30: DEVICE SERIAL: " + str(sps.read_device_serial()))
+	if sps.read_device_serial() == sps.SERIAL_NUMBER_ERROR:
+		raise Exception("SPS30: SERIAL NUMBER CRC ERROR!")
+	else:
+		print("SPS30: DEVICE SERIAL: " + str(sps.read_device_serial()))
 
-    sps.set_auto_cleaning_interval(604800) # default 604800, set 0 to disable auto-cleaning
+	sps.set_auto_cleaning_interval(604800) # default 604800, set 0 to disable auto-cleaning
 
-    sps.device_reset() # device has to be powered-down or reset to check new auto-cleaning interval
-    if sps.read_auto_cleaning_interval() == sps.AUTO_CLN_INTERVAL_ERROR: # or returns the interval in seconds
-        raise Exception("SPS30: AUTO-CLEANING INTERVAL CRC ERROR!")
-    else:
-        print("SPS30: AUTO-CLEANING INTERVAL: " + str(sps.read_auto_cleaning_interval()))
+	sps.device_reset() # device has to be powered-down or reset to check new auto-cleaning interval
+	if sps.read_auto_cleaning_interval() == sps.AUTO_CLN_INTERVAL_ERROR: # or returns the interval in seconds
+		raise Exception("SPS30: AUTO-CLEANING INTERVAL CRC ERROR!")
+	else:
+		print("SPS30: AUTO-CLEANING INTERVAL: " + str(sps.read_auto_cleaning_interval()))
 
-    time.sleep(5)
-    sps.start_measurement()
-    time.sleep(5)
+	time.sleep(5)
+	sps.start_measurement()
+	time.sleep(5)
 
-    print("SPS30: Measuring")
-    if not sps.read_data_ready_flag():
-        if sps.read_data_ready_flag() == sps.DATA_READY_FLAG_ERROR:
-            raise Exception("SPS30: DATA-READY FLAG CRC ERROR!")
-    elif sps.read_measured_values() == sps.MEASURED_VALUES_ERROR:
-        raise Exception("SPS30: MEASURED VALUES CRC ERROR!")
-    sps30_pm1 = str("PM1.0: %0.1f µg/m3" % sps.dict_values['pm1p0'])
-    sps30_pm2p5 = str("PM2.5: %0.1f µg/m3" % sps.dict_values['pm2p5'])
-    sps30_pm10 = str("PM10 : %0.1f µg/m3" % sps.dict_values['pm10p0'])
-    sps30_nc1 = str("NC1.0: %0.1f 1/cm3" % sps.dict_values['nc1p0'])
-    sps30_nc4 = str("NC4.0: %0.1f 1/cm3" % sps.dict_values['nc4p0'])
-    sps30_typ = str("Typical Particle: %0.1f µm" % sps.dict_values['typical'])
-    print(f"SPS30 readouts:")
-    print(sps30_pm1+" "+sps30_pm2p5+" "+sps30_pm10+" "+sps30_nc1+" "+sps30_nc4+" "+sps30_typ)
-    # print(sps30_pm2p5)
-    # print(sps30_pm10)
-    # print(sps30_nc1)
-    # print(sps30_nc4)
-    # print(sps30_typ)
+	print("SPS30: Measuring")
+	if not sps.read_data_ready_flag():
+		if sps.read_data_ready_flag() == sps.DATA_READY_FLAG_ERROR:
+			raise Exception("SPS30: DATA-READY FLAG CRC ERROR!")
+	elif sps.read_measured_values() == sps.MEASURED_VALUES_ERROR:
+		raise Exception("SPS30: MEASURED VALUES CRC ERROR!")
+	sps30_pm1 = str("PM1.0: %0.1f µg/m3" % sps.dict_values['pm1p0'])
+	sps30_pm2p5 = str("PM2.5: %0.1f µg/m3" % sps.dict_values['pm2p5'])
+	sps30_pm10 = str("PM10 : %0.1f µg/m3" % sps.dict_values['pm10p0'])
+	sps30_nc1 = str("NC1.0: %0.1f 1/cm3" % sps.dict_values['nc1p0'])
+	sps30_nc4 = str("NC4.0: %0.1f 1/cm3" % sps.dict_values['nc4p0'])
+	sps30_typ = str("Typical Particle: %0.1f µm" % sps.dict_values['typical'])
+	print(f"SPS30 readouts:")
+	print(sps30_pm1+" "+sps30_pm2p5+" "+sps30_pm10+" "+sps30_nc1+" "+sps30_nc4+" "+sps30_typ)
+	# print(sps30_pm2p5)
+	# print(sps30_pm10)
+	# print(sps30_nc1)
+	# print(sps30_nc4)
+	# print(sps30_typ)
 
 def test_t6713():
 	obj_6713 = T6713()
@@ -313,18 +313,18 @@ def test_oled():
 	x = 0
 
 def main():
-    # Warm up GPIO
-    gpio_start()
-    print(f"Starting {bcolors.HIGH}GPIO{bcolors.TEXT}: {bcolors.PASS}PASS{bcolors.TEXT}")
-    # Start menu
-    exit_sel = False
-    options = ["SHTC3", "SPS30", "T6713", "OLED", "All", "exit"]
-    terminal_menu = TerminalMenu(options)
-    while not (exit_sel):
-        menu_entry_index = terminal_menu.show()
-        exit_sel = (options[menu_entry_index] == "exit")
-        test_all = (options[menu_entry_index] == "All")
-        if not (exit_sel): 
+	# Warm up GPIO
+	gpio_start()
+	print(f"Starting {bcolors.HIGH}GPIO{bcolors.TEXT}: {bcolors.PASS}PASS{bcolors.TEXT}")
+	# Start menu
+	exit_sel = False
+	options = ["SHTC3", "SPS30", "T6713", "OLED", "All", "exit"]
+	terminal_menu = TerminalMenu(options)
+	while not (exit_sel):
+		menu_entry_index = terminal_menu.show()
+		exit_sel = (options[menu_entry_index] == "exit")
+		test_all = (options[menu_entry_index] == "All")
+		if not (exit_sel): 
 			if (test_all):
 				for i in ["SHTC3", "SPS30", "T6713", "OLED"]:
 					test_good = test(i)
