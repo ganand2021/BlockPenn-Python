1ce4c38c08342ecb554764d1feecaf92eceb68c7
ganand2021 <90202590+ganand2021@users.noreply.github.com>
2023-05-09 13:18:18 -0400

    Add files via upload

--- Kasa/kasa_api_test.py
+++ Kasa/kasa_api_test.py
@@ -1,107 +1,111 @@
 #!/usr/bin/python
 import requests, secrets, json
-from dotenv import dotenv_values
+# from dotenv import dotenv_values
 
-config = dotenv_values("kasa.env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
+# config = dotenv_values("kasa.env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
 
-api_url = config["API_URL"]
-username = config["USERNAME"]
-password = config["PASSWORD"]
+# api_url = config["API_URL"]
+# username = config["USERNAME"]
+# password = config["PASSWORD"]
 
-def create_random_uuid():
-    uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-    uuid = list(uuid)
+class Kasa(object):
+    def __init__(self):
+        self.api_url = "https://wap.tplinkcloud.com"
+    
+    def create_random_uuid(self):
+        uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+        uuid = list(uuid)
 
-    uuid_sec = secrets.token_hex(36)
+        uuid_sec = secrets.token_hex(36)
 
-    for uuid_index,uuid_char in enumerate(uuid):
-        r = uuid_sec[uuid_index]
-        d = int(r,16) & 0x3 | 0x8
-    #    (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
-        if (uuid_char == "x"):
-            uuid[uuid_index] = r
-        elif (uuid_char == "y"):
-            uuid[uuid_index] = format(d,'01x')
+        for uuid_index,uuid_char in enumerate(uuid):
+            r = uuid_sec[uuid_index]
+            d = int(r,16) & 0x3 | 0x8
+        #    (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+            if (uuid_char == "x"):
+                uuid[uuid_index] = r
+            elif (uuid_char == "y"):
+                uuid[uuid_index] = format(d,'01x')
 
-    uuid = "".join(uuid)
-    return uuid
+        uuid = "".join(uuid)
+        return uuid
 
-def get_auth_token(uuid, username, password):
-    auth_obj = {
-        "method": "login", 
-        "params": {
-            "appType": "Kasa_Android",
-            "cloudUserName": username,
-            "cloudPassword": password,
-            "terminalUUID": uuid,
+    def get_auth_token(self, uuid, username, password):
+        auth_obj = {
+            "method": "login", 
+            "params": {
+                "appType": "Kasa_Android",
+                "cloudUserName": username,
+                "cloudPassword": password,
+                "terminalUUID": uuid,
+            }
         }
-    }
 
-    response = requests.post(api_url+"/", json=auth_obj)
-#    if (response.status_code == 200): print("Auth success!")
-    kasa_token = response.json()["result"]["token"]
-    return response.status_code, kasa_token
+        response = requests.post(self.api_url+"/", json=auth_obj)
+    #    if (response.status_code == 200): print("Auth success!")
+        kasa_token = response.json()["result"]["token"]
+        return response.status_code, kasa_token
 
-def get_dev_list(token):
-    dev_obj = {"method": "getDeviceList"}
-    response = requests.post(api_url+"?token="+token, json=dev_obj)
-    return response.status_code, response.json()["error_code"], response.json()["result"]["deviceList"]
+    def get_dev_list(self, token):
+        dev_obj = {"method": "getDeviceList"}
+        response = requests.post(self.api_url+"?token="+token, json=dev_obj)
+        return response.status_code, response.json()["error_code"], response.json()["result"]["deviceList"]
 
-def set_dev_state(token, sel_device_id, sel_device_state):
-    set_obj = {
-        "method": "passthrough", "params": {
-            "deviceId": sel_device_id,
-            "requestData": "{\"system\":{\"set_relay_state\":{\"state\":" + str(sel_device_state) + "}}}"
+    def set_dev_state(self, token, sel_device_id, sel_device_state):
+        set_obj = {
+            "method": "passthrough", "params": {
+                "deviceId": sel_device_id,
+                "requestData": "{\"system\":{\"set_relay_state\":{\"state\":" + str(sel_device_state) + "}}}"
+            }
         }
-    }
-    response = requests.post(api_url+"?token="+token, json=set_obj)
-    return response.status_code, response.json()["error_code"]
+        response = requests.post(self.api_url+"?token="+token, json=set_obj)
+        return response.status_code, response.json()["error_code"]
 
-def set_dev_state_emeter(token, sel_device_id):
-    set_obj = {
-        "method": "passthrough", "params": {
-            "deviceId": sel_device_id,
-            "requestData": "{\"system\":{\"get_sysinfo\":null},\"emeter\":{\"get_realtime\":null}}"
+    def set_dev_state_emeter(self, token, sel_device_id):
+        set_obj = {
+            "method": "passthrough", "params": {
+                "deviceId": sel_device_id,
+                "requestData": "{\"system\":{\"get_sysinfo\":null},\"emeter\":{\"get_realtime\":null}}"
+            }
         }
-    }
-    response = requests.post(api_url+"?token="+token, json=set_obj)
-    return response.status_code, response.json()["error_code"], response.json()
+        response = requests.post(self.api_url+"?token="+token, json=set_obj)
+        return response.status_code, response.json()["error_code"], response.json()
 
-# Authenticate and get token
-uuid = create_random_uuid()
-print("uuid:",uuid)
-[response_code, kasa_token] = get_auth_token(uuid, username, password)
-if (response_code == 200): print("Auth success!")
-print("kasa_token", kasa_token)
-# kasa_token = set the token here if you already have one
-# Get device list
-[response_code, err_code, dev_list] = get_dev_list(kasa_token)
-if (response_code == 200): print("List success!")
-if (err_code == 0): print("List has no errors")
-print("Identified # of devices:",len(dev_list))
+# # Authenticate and get token
+# uuid = create_random_uuid()
+# print("uuid:",uuid)
+# [response_code, kasa_token] = get_auth_token(uuid, username, password)
+# if (response_code == 200): print("Auth success!")
+# print("kasa_token", kasa_token)
+# # kasa_token = set the token here if you already have one
+# # Get device list
+# [response_code, err_code, dev_list] = get_dev_list(kasa_token)
+# if (response_code == 200): print("List success!")
+# if (err_code == 0): print("List has no errors")
+# print("Identified # of devices:",len(dev_list))
 
-# Select device and turn it off
-sel_device = dev_list[0]
-print("sel_device", sel_device["deviceId"])
-sel_device_id = sel_device["deviceId"]
-sel_device_state = 1
-[response_code, err_code] = set_dev_state(kasa_token, sel_device_id, sel_device_state)
-if (response_code == 200): print("Set success!")
-if (err_code == 0): print("Set has no errors")
+# # Select device and turn it off
+# sel_device = dev_list[0]
+# print("sel_device", sel_device["deviceId"])
+# sel_device_id = sel_device["deviceId"]
+# sel_device_state = 1
+# [response_code, err_code] = set_dev_state(kasa_token, sel_device_id, sel_device_state)
+# if (response_code == 200): print("Set success!")
+# if (err_code == 0): print("Set has no errors")
 
-[response_code, err_code, json_resp] = set_dev_state_emeter(kasa_token, sel_device_id)
-if (response_code == 200): print("Set success!")
-if (err_code == 0): print("Set has no errors")
+# [response_code, err_code, json_resp] = set_dev_state_emeter(kasa_token, sel_device_id)
+# if (response_code == 200): print("Set success!")
+# if (err_code == 0): print("Set has no errors")
 
-dev_ma = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['current_ma']
-dev_mv = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['voltage_mv']
-dev_mw = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['power_mw']
-dev_wh = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['total_wh']
-dev_e_err = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['err_code']
-print(str("dev_ma: %0.2f A" % (dev_ma/1000)))
-print(str("dev_mv: %0.2f V" % (dev_mv/1000)))
-print(str("dev_mw: %0.2f W" % (dev_mw/1000)))
-print(str("dev_wh: %0.2f W/H" % (dev_wh/1000)))
+# dev_ma = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['current_ma']
+# dev_mv = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['voltage_mv']
+# dev_mw = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['power_mw']
+# dev_wh = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['total_wh']
+# dev_e_err = json.loads(json_resp['result']['responseData'])['emeter']['get_realtime']['err_code']
+# print(str("dev_ma: %0.2f A" % (dev_ma/1000)))
+# print(str("dev_mv: %0.2f V" % (dev_mv/1000)))
+# print(str("dev_mw: %0.2f W" % (dev_mw/1000)))
+# print(str("dev_wh: %0.2f W/H" % (dev_wh/1000)))
 #  {'error_code': 0, 
 #  'result': 
 #     {'responseData': 
